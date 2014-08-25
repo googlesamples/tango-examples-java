a2d255bf63d1fda7593a0a4fe00bb97bc48271d2
samstern <samstern@google.com>
2014-08-25 15:35:29 -0700

    Merged style changes into Start and Complete

--- JMotionTrackingSample_Start/src/com/projecttango/jmotiontrackingsample/Renderables/Axis.java
+++ JMotionTrackingSample_Start/src/com/projecttango/jmotiontrackingsample/Renderables/Axis.java
@@ -4,21 +4,16 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
-import java.lang.Math;
-
 import com.projecttango.jmotiontrackingsample.MTGLRenderer;
 
 import android.opengl.GLES20;
 import android.opengl.Matrix;
-import android.util.Log;
 
 public class Axis {
 
-	private float[] mTranslation = new float[3];
-	private float[] mQuaternion = new float[4];
-
-	private FloatBuffer mVertexBuffer, mColorBuffer;
-	private final String vertexShaderCode =
+	private static final int COORDS_PER_VERTEX = 3;
+	
+	private static final String sVertexShaderCode =
 			"uniform mat4 uMVPMatrix;"
 			+ "attribute vec4 vPosition;"
 			+ "attribute vec4 aColor;"
@@ -28,116 +23,134 @@ public class Axis {
 			+ "gl_Position = uMVPMatrix * vPosition;"
 			+ "}";
 
-	private final String fragmentShaderCode = 
+	private static final String sFragmentShaderCode = 
 			"precision mediump float;"
 			+ "varying vec4 vColor;" 
 			+ "void main() {"
 			+ "gl_FragColor = vColor;" + 
 			"}";
+	
+	private float[] mTranslation = new float[3];
+	private float[] mQuaternion = new float[4];
+	private FloatBuffer mVertexBuffer, mColorBuffer;
 
-	private float vertices[] = {  0.0f, 0.0f, 0.0f,
+	private float mVertices[] = {  
+			0.0f, 0.0f, 0.0f,
 		    1.0f, 0.0f, 0.0f,
+		    
 		    0.0f, 0.0f, 0.0f,
 		    0.0f, 1.0f, 0.0f,
+		    
 		    0.0f, 0.0f, 0.0f,
-		    0.0f, 0.0f, 1.0f  };
+		    0.0f, 0.0f, 1.0f};
 
-	private float colors[] = { 1.0f, 0.0f, 0.0f, 1.0f,
+	private float mColors[] = { 
+			1.0f, 0.0f, 0.0f, 1.0f,
 			1.0f, 0.0f, 0.0f, 1.0f,
 			
 			0.0f, 1.0f, 0.0f, 1.0f,
 			0.0f, 1.0f, 0.0f, 1.0f,
 			
 			0.0f, 0.0f, 1.0f, 1.0f,
-			0.0f, 0.0f, 1.0f, 1.0f, };
+			0.0f, 0.0f, 1.0f, 1.0f};
 
-	private float[] modelMatrix = new float[16];
-	private float[] mvMatrix = new float[16];
-	private float[] mvpMatrix = new float[16];
+	private float[] mModelMatrix = new float[16];
+	private float[] mMvMatrix = new float[16];
+	private float[] mMvpMatrix = new float[16];
 
 	private final int mProgram;
 	private int mPosHandle, mColorHandle;
-	static final int COORDS_PER_VERTEX = 3;
 	private int mMVPMatrixHandle;
 
 	public Axis() {
-		Matrix.setIdentityM(modelMatrix, 0);
+		// Set model matrix to the identity
+		Matrix.setIdentityM(mModelMatrix, 0);
 
-		ByteBuffer byteBuf = ByteBuffer.allocateDirect(vertices.length * 4);
+		// Put vertices into a vertex buffer
+		ByteBuffer byteBuf = ByteBuffer.allocateDirect(mVertices.length * 4);
 		byteBuf.order(ByteOrder.nativeOrder());
 		mVertexBuffer = byteBuf.asFloatBuffer();
-		mVertexBuffer.put(vertices);
+		mVertexBuffer.put(mVertices);
 		mVertexBuffer.position(0);
 
-		ByteBuffer cByteBuff = ByteBuffer.allocateDirect(colors.length * 4);
+		// Put colors into a color buffer
+		ByteBuffer cByteBuff = ByteBuffer.allocateDirect(mColors.length * 4);
 		cByteBuff.order(ByteOrder.nativeOrder());
 		mColorBuffer = cByteBuff.asFloatBuffer();
-		mColorBuffer.put(colors);
+		mColorBuffer.put(mColors);
 		mColorBuffer.position(0);
 
-		int vertexShader = MTGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
-				vertexShaderCode);
-		int fragShader = MTGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
-				fragmentShaderCode);
+		// Load the vertex and fragment shaders, then link the program
+		int vertexShader = MTGLRenderer.loadShader(GLES20.GL_VERTEX_SHADER, sVertexShaderCode);
+		int fragShader = MTGLRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, sFragmentShaderCode);
 		mProgram = GLES20.glCreateProgram();
 		GLES20.glAttachShader(mProgram, vertexShader);
 		GLES20.glAttachShader(mProgram, fragShader);
 		GLES20.glLinkProgram(mProgram);
 	}
 
+	/**
+	 * Updates the model matrix (rotation and translation) of the Axis.
+	 * @param translation a three-element array of translation data.
+	 * @param quaternion a four-element array of rotation data.
+	 */
 	public void updateModelMatrix(float[] translation, float[] quaternion) {
-
 		mTranslation = translation;
 		mQuaternion = quaternion;
 		
-		float[] openglQuaternion = MathUtils.ConvertQuaternionToOpenGl(quaternion);
+		float[] openglQuaternion = MathUtils.convertQuaternionToOpenGl(quaternion);
 		float[] quaternionMatrix = new float[16];
 		
+		// Update the model matrix with translation data
 		quaternionMatrix = MathUtils.quaternionM(openglQuaternion);
-		//quaternionMatrix = MathUtils.quaternionM(quaternion);
-		Matrix.setIdentityM(modelMatrix, 0);
-		Matrix.translateM(modelMatrix, 0, translation[0], translation[2],
-				-translation[1]);
+		Matrix.setIdentityM(mModelMatrix, 0);
+		Matrix.translateM(mModelMatrix, 0, translation[0], translation[2], -translation[1]);
 
-		float[] mTempMatrix = new float[16];
-		Matrix.setIdentityM(mTempMatrix, 0);
-
+		// Update the model matrix with rotation data
 		if (quaternionMatrix != null) {
-			Matrix.multiplyMM(mTempMatrix, 0, quaternionMatrix, 0, modelMatrix,
-					0);
-
-			modelMatrix = mTempMatrix;
+			float[] mTempMatrix = new float[16];
+			Matrix.setIdentityM(mTempMatrix, 0);
+			
+			Matrix.multiplyMM(mTempMatrix, 0, quaternionMatrix, 0, mModelMatrix, 0);
+			mModelMatrix = mTempMatrix;
 		}
 	};
 
 	public void updateViewMatrix(float[] viewMatrix) {
-		Matrix.setLookAtM(viewMatrix, 0, 0,
-				 5.0f, 5.0f,
-				mTranslation[0], mTranslation[1], mTranslation[2], 0, 1, 0);
+		Matrix.setLookAtM(viewMatrix, 0, 0, 5.0f, 5.0f, mTranslation[0], mTranslation[1], 
+				mTranslation[2], 0, 1, 0);
 	}
 
+	/**
+	 * Applies the view and projection matrices and draws the Axis.
+	 * @param viewMatrix the view matrix to map from world space to camera space.
+	 * @param projectionMatrix the projection matrix to map from camera space to screen space.
+	 */
 	public void draw(float[] viewMatrix, float[] projectionMatrix) {
 
 		GLES20.glUseProgram(mProgram);
 		//updateViewMatrix(viewMatrix);
 
-		Matrix.setIdentityM(mvMatrix, 0);
-		Matrix.setIdentityM(mvpMatrix, 0);
-		Matrix.multiplyMM(mvMatrix, 0, viewMatrix, 0, modelMatrix, 0);
-		Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, mvMatrix, 0);
+		// Compose the model, view, and projection matrices into a single m-v-p matrix
+		Matrix.setIdentityM(mMvMatrix, 0);
+		Matrix.setIdentityM(mMvpMatrix, 0);
+		Matrix.multiplyMM(mMvMatrix, 0, viewMatrix, 0, mModelMatrix, 0);
+		Matrix.multiplyMM(mMvpMatrix, 0, projectionMatrix, 0, mMvMatrix, 0);
 
+		// Load vertex attribute data
 		mPosHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
-		GLES20.glVertexAttribPointer(mPosHandle, COORDS_PER_VERTEX,
-				GLES20.GL_FLOAT, false, 0, mVertexBuffer);
+		GLES20.glVertexAttribPointer(mPosHandle, COORDS_PER_VERTEX, GLES20.GL_FLOAT, false, 0, 
+				mVertexBuffer);
 		GLES20.glEnableVertexAttribArray(mPosHandle);
 
+		// Load color attribute data
 		mColorHandle = GLES20.glGetAttribLocation(mProgram, "aColor");
-		GLES20.glVertexAttribPointer(mColorHandle, 4, GLES20.GL_FLOAT, false,
-				0, mColorBuffer);
+		GLES20.glVertexAttribPointer(mColorHandle, 4, GLES20.GL_FLOAT, false, 0, mColorBuffer);
 		GLES20.glEnableVertexAttribArray(mColorHandle);
 
+		// Draw the Axis
 		mMVPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
-		GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mvpMatrix, 0);
+		GLES20.glUniformMatrix4fv(mMVPMatrixHandle, 1, false, mMvpMatrix, 0);
 		GLES20.glLineWidth(5);
 		GLES20.glDrawArrays(GLES20.GL_LINES, 0, 6);
 
